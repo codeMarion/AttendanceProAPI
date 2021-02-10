d63de92b68c1c46aaff18a021a7e53b1ac7ec222
Marion Laanemae <ml553@sussex.ac.uk>
2021-02-10 01:41:36 +0000

    Created BlobStorage service which communicates with the blob storages which stores all the email communications.

---
+++ Services/BlobStorageService.cs
@@ -0,0 +1,50 @@
+﻿using AttendanceProAPI.Models;
+using AttendanceProAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceProAPI.Services
+{
+    public class BlobStorageService : IBlobStorageService
+    {
+        private SendGridSettings settings;
+        private CloudBlobClient blobClient;
+        private CloudStorageAccount storageAccount;
+        private CloudBlobContainer blobContainer;
+        public BlobStorageService(IOptions<SendGridSettings> settings)
+        {
+            this.settings = settings.Value;
+            storageAccount = CloudStorageAccount.Parse(this.settings.BlobConnectionString);
+            blobClient = storageAccount.CreateCloudBlobClient();
+            blobContainer = blobClient.GetContainerReference(this.settings.BlobContainer);
+        }
+        public async Task<IActionResult> AddNewEmailData(SendGridEmailRequest email, string folder, string file)
+        {
+            CloudBlockBlob blob = blobContainer.GetBlockBlobReference($"{folder}/{file}");
+            await blob.UploadTextAsync(JsonConvert.SerializeObject(email));
+            return new OkResult();
+        }
+
+        public async Task<IActionResult> GetEmails(string folder)
+        {
+            List<SendGridEmailRequest> emails = new List<SendGridEmailRequest>();
+            BlobContinuationToken blobContinuationToken = null;
+            BlobResultSegment blobResponse = await blobContainer.ListBlobsSegmentedAsync($"{folder}/", true, BlobListingDetails.None, null, blobContinuationToken, null, null);
+            foreach (CloudBlockBlob blob in blobResponse.Results.OfType<CloudBlockBlob>())
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                await blob.DownloadToStreamAsync(memoryStream);
+                emails.Add(JsonConvert.DeserializeObject<SendGridEmailRequest>(System.Text.Encoding.UTF8.GetString(memoryStream.ToArray())));
+            }
+            return new OkObjectResult(emails);
+        }
+    }
+}
---
+++ Services/Interfaces/IBlobStorageService.cs
@@ -0,0 +1,15 @@
+﻿using AttendanceProAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttendanceProAPI.Services.Interfaces
+{
+    public interface IBlobStorageService
+    {
+        Task<IActionResult> AddNewEmailData(SendGridEmailRequest email, string folder, string file);
+        Task<IActionResult> GetEmails(string folder);
+    }
+}
